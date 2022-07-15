139216877eb11f1031198a534fa1dedd831cfbb0
Ananya Rao <ananyarao610@gmail.com>
2022-07-15 23:04:07 +0530

    changes

--- face_recog.py
+++ face_recog.py
@@ -3,13 +3,13 @@ import cv2
 import numpy as np
 
 video_capture = cv2.VideoCapture(0)
-image_list = ["images/person1.jpg", "images/person2.jpg","images/person3.jpeg"]
+image_list = ["images/person1.jpg", "images/person2.jpg"]
 known_faces =[]
 
 known_face_names = [
     "Ananya Rao",
     "Sreeharsha A"
-    "Park Jimin"
+    
 ]
 
 for i in image_list:
@@ -26,24 +26,28 @@ process_this_frame = True
 
 while True:
     
-    _, frame = video_capture.read()
+    ret, frame = video_capture.read()
 
     
     if process_this_frame:
         
-        frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-        frame = frame[:, :, ::-1]
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
         
-       
-        face_locations = face_recognition.face_locations(frame)
-        face_encodings = face_recognition.face_encodings(frame, face_locations)
+        rgb_small_frame = small_frame[:, :, ::-1]
+        
+        
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
         face_names = []
-        for i in face_encodings:
-            matches = face_recognition.compare_faces(known_faces, i)
+        for face_encoding in face_encodings:
+            
+            matches = face_recognition.compare_faces(known_faces, face_encoding)
             name = "Unknown"
 
-            face_distances = face_recognition.face_distance(known_faces, i)
+            
+            face_distances = face_recognition.face_distance(known_faces, face_encoding)
             best_match_index = np.argmin(face_distances)
             if matches[best_match_index]:
                 name = known_face_names[best_match_index]
@@ -60,30 +64,30 @@ while True:
         right *= 4
         bottom *= 4
         left *= 4
-        #print(name)
+
+        
         if name == "Unknown":
             cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-            
+            # Draw a label with a name below the face
             cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
             font = cv2.FONT_HERSHEY_DUPLEX
             cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
         else:
             cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
 
-            
+            # Draw a label with a name below the face
             cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 255, 0), cv2.FILLED)
             font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (0,0,0), 1)
 
 
-    
+    # Display the resulting image
     cv2.imshow('Video', frame)
 
-    
+   
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
 
 video_capture.release()
 cv2.destroyAllWindows()
